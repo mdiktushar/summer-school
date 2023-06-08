c039b74aa93f276f6731aba3b71d234061596eae
mdiktushar <md.tushar1804@gmail.com>
2023-06-08 21:03:51 +0600

    singup page react-hooks-form

--- src/Pages/Auth/SignUp.jsx
+++ src/Pages/Auth/SignUp.jsx
@@ -1,21 +1,38 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  // submit function
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="hero min-h-[700px] bg-base-200">
       <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-        <div className="card-body">
+        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Email</span>
+              <span className="label-text">Name</span>
             </label>
             <input
               type="text"
               placeholder="Name"
               name="name"
               className="input input-bordered"
+              {...register("name", { required: true })}
             />
+            {errors.name && (
+              <span className="text-red-600">Name is required</span>
+            )}
           </div>
           <div className="form-control">
             <label className="label">
@@ -26,18 +43,39 @@ const SignUp = () => {
               placeholder="Email"
               name="email"
               className="input input-bordered"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Invalid email address",
+                },
+              })}
             />
+            {errors.email && (
+              <span className="text-red-600">{errors.email.message}</span>
+            )}
           </div>
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Email</span>
+              <span className="label-text">Photo URL</span>
             </label>
             <input
               type="text"
               placeholder="PhotoURL"
               name="photoURL"
               className="input input-bordered"
+              {...register("photoURL", {
+                required: "Photo URL is required",
+                pattern: {
+                  value:
+                    /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/,
+                  message: "Invalid URL",
+                },
+              })}
             />
+            {errors.photoURL && (
+              <span className="text-red-600">{errors.photoURL.message}</span>
+            )}
           </div>
           <div className="form-control">
             <label className="label">
@@ -48,7 +86,24 @@ const SignUp = () => {
               placeholder="Password"
               name="password"
               className="input input-bordered"
+              {...register("password", {
+                required: true,
+                minLength: 6,
+                pattern: /(?=.*[A-Z])(?=.*[!@#$&*])/,
+              })}
             />
+            {errors.password?.type === "required" && (
+              <p className="text-red-600">Password is required</p>
+            )}
+            {errors.password?.type === "minLength" && (
+              <p className="text-red-600">Password must be 6 characters</p>
+            )}
+            {errors.password?.type === "pattern" && (
+              <p className="text-red-600">
+                Password must have at least one Uppercase letter & one special
+                character.
+              </p>
+            )}
           </div>
           <div className="form-control">
             <label className="label">
@@ -57,9 +112,20 @@ const SignUp = () => {
             <input
               type="password"
               placeholder="config Password"
-              name="configPassword"
+              name="confirm_password"
               className="input input-bordered"
+              {...register("confirm_password", {
+                required: true,
+                validate: (val) => {
+                  if (watch("password") != val) {
+                    return "Your passwords do no match";
+                  }
+                },
+              })}
             />
+            {errors.confirm_password && (
+              <p className="text-red-600">{errors.confirm_password.message}</p>
+            )}
           </div>
           <div className="form-control mt-6">
             <button className="btn btn-primary">SignUp</button>
@@ -69,7 +135,7 @@ const SignUp = () => {
               Already have an account? <Link to={`/login`}>Go to Login</Link>
             </p>
           </label>
-        </div>
+        </form>
       </div>
     </div>
   );
