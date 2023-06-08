9a4f2c26ca3c0371ce681ae9aa907b18b532a38e
mdiktushar <md.tushar1804@gmail.com>
2023-06-08 15:16:50 +0600

    firebse config file

--- src/Pages/Auth/SignUp.jsx
+++ src/Pages/Auth/SignUp.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div className="hero min-h-[600px] bg-base-200">
+    <div className="hero min-h-[800px] bg-base-200">
       <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
         <div className="card-body">
           <div className="form-control">
