20f34ffafd1b45ad1eda5f58e03823c5bd7b0d3f
mdiktushar <md.tushar1804@gmail.com>
2023-06-11 16:25:53 +0600

    banner

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Slider from './components/slider';
 
 const Home = () => {
     return (
         <div>
+            <Slider />
            <h2>Home</h2>
         </div>
     );
